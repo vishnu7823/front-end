3a5437c2b2b1b9bd41a165fc41b27b81f9fdc826
M Vishnu vardhan <122662900+vishnuvardhan7823@users.noreply.github.com>
2023-07-14 21:40:20 +0530

    operators

---
+++ script.js
@@ -0,0 +1,85 @@
+/*operators
+-----------------
+
+arithmetic operators: + - *,/ % ++ -- **  */
+
+// let a=3;
+
+// let x=(a**3)
+
+// console.log("x=" +x)
+
+
+// let b=5;
+// let y=(--b)
+// console.log(+b)
+
+// let c=5;
+// let z=(c--)
+// console.log(+z)
+
+
+// let d=5
+// let p=(d++)
+// console.log(+p)
+
+//asssignment operators: = += -= *= /= %= **=//
+
+// let a=10
+// let x=(a+=3)
+// console.log(+x)
+
+// let b=10
+// let y=(b-=3)
+// console.log(+y)
+
+// let c=10
+// let z=(c*=3)
+// console.log(+z)
+
+// let d=10
+// let p=(d/=5)
+// console.log(+p)
+
+// let e=10
+// let q=(e%=3)
+// console.log(+q)
+
+// let f=10
+// let r=(f**=3)
+// console.log(+r)
+
+
+//comaprison operators:== === != !== > < >= <= ??
+
+// let a= 5
+// let x=(a==10)
+// console.log(+x)
+
+// let b=10
+// let y=(b>=30)
+// console.log(+y)
+
+
+//logical operator : && || ! 
+
+
+// let a=100
+// let x=(a>50)
+// console.log( +x)
+
+//bitwise operator: & | ` ^ << >> >>>
+
+
+//user input
+
+// let name=prompt("whats your name")
+
+// console.log('hi',name)
+
+
+let tickets=prompt('how many tickets dpo you want')
+console.log('you should pay',Number (tickets)*20)
+
+let a=prompt("entyer a number")
+console.log(Number(a)+10)
