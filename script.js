9701aef95e35ee3e48bcd1131bb5b6971f42e5bd
M Vishnu vardhan <122662900+vishnuvardhan7823@users.noreply.github.com>
2023-07-15 09:30:00 +0530

    string

---
+++ script.js
@@ -0,0 +1,17 @@
+let str1= "    strive not to 7787*& a success strive,   "
+let str2="but rather to be a value"
+
+//partioning:
+
+//slice(start,end)
+//substring(start,end)
+//substr(start,length)
+
+//replace string:
+//str1.replace("strive","aim")
+
+//str1.toUpperCase()
+
+let name='bharathi'
+Math.random(name)
+console.log(name)
